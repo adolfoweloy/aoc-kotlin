1cd71e0a450c3f2ac952dcf510d8238d373dccbf
Adolfo <1496547+adolfoweloy@users.noreply.github.com>
2023-12-10 14:54:42 +1100

    finish day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,4 @@
+import kotlin.math.max
 
 val rule = mapOf(
     "red" to 12,
@@ -5,17 +6,31 @@ val rule = mapOf(
     "blue" to 14
 )
 
+val colors = listOf("red", "green", "blue")
+
 fun main() {
     fun part1(input: List<String>): Int {
         val games = input.map { Row(it).parseGame() }
         return games.filterNot { it.cracks() }.sumOf { it.id }
     }
 
-    fun part2(input: List<String>) = 9
+    fun part2(input: List<String>): Int {
+        val games = input.map { Row(it).parseGame() }
+
+        fun getMapWithMaxValue(acc: Map<String, Int>, cubeSet: Map<String, Int>) =
+            colors.associateWith { color -> max(acc[color] ?: 0, cubeSet[color] ?: 0)  }
+
+        return games.sumOf {
+            it.subsets.fold(initial = mapOf<String, Int>()) { acc, b -> getMapWithMaxValue(acc, b) }
+                .map { (_, value) -> value }
+                .reduce { acc, value -> acc * value }
+        }
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
@@ -26,21 +41,19 @@ fun Game.cracks() = subsets.any { it.cracks() }
 
 data class Row(val value: String)
 
-typealias Subsets = List<CubeSet>
-typealias CubeSet = Pair<String, Int>
+typealias Subsets = Map<String, Int>
+typealias RevealedPair = Pair<String, Int>
 
 data class Game(val id: Int, val subsets: List<Subsets>)
 
 fun Subsets.cracks() = any {
-    val maxPossibleCubes = rule[it.first]
-    if (maxPossibleCubes != null) {
-        it.cracks(maxPossibleCubes)
+    val maxCubes = rule[it.key]
+    if (maxCubes != null) {
+        it.value > maxCubes
     } else {
         true
     }
 }
-
-fun CubeSet.cracks(possibleAmount: Int): Boolean = second > possibleAmount
 
 fun Row.parseGame(): Game {
     val text = value.split(":")
@@ -53,20 +66,24 @@ fun String.game(): List<Subsets> = subsetsOfCubesString().map { it.parseSubsets(
 
 fun String.subsetsOfCubesString() = split(";")
 
-fun String.parseSubsets(): Subsets = splitSubsetString().map { it.parseSubset() }
+fun String.parseSubsets(): Subsets = splitSubsetString()
+    .map { it.parseSubset() }
+    .associateBy(keySelector = { it.first }, valueTransform = { it.second })
 
 fun String.splitSubsetString() = split(",")
 
-fun String.parseSubset(): CubeSet = mapCubeColorToSubsetSize(
-    fold(listOf("", "")) { acc, c -> parseSubsetOfCubes(acc, c) }
+fun String.parseSubset(): RevealedPair = mapCubeColorToSubsetSize(
+    fold(initial = listOf("", "")) { acc, c -> parseSubsetOfCubes(acc, c) }
 )
 
-fun mapCubeColorToSubsetSize(listDraw: List<String>): CubeSet = listDraw[1] to listDraw[0].toInt()
+const val cubes = 0
+const val color = 1
 
-fun parseSubsetOfCubes(acc: List<String>, c: Char) =
+fun parseSubsetOfCubes(cubesAndColor: List<String>, c: Char) =
     when {
-        c.isDigit() -> listOf(acc[0] + c.toString(), acc[1])
-        c == ' ' -> acc
-        else -> listOf(acc[0], acc[1] + c)
+        c.isDigit() -> listOf(cubesAndColor[cubes] + c.toString(), cubesAndColor[color])
+        c == ' ' -> cubesAndColor
+        else -> listOf(cubesAndColor[cubes], cubesAndColor[color] + c)
     }
 
+fun mapCubeColorToSubsetSize(cubesAndColors: List<String>): RevealedPair = cubesAndColors[color] to cubesAndColors[cubes].toInt()
