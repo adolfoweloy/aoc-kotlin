9d4fb797ed111e1192c0e545a707afc66c6d6542
Adolfo <1496547+adolfoweloy@users.noreply.github.com>
2023-12-10 14:54:42 +1100

    day 03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,23 @@
 fun main() {
-    fun part1(input: List<String>) =
-        input.asSequence().mapIndexed { idx, row -> row.parse(idx).filterOutSeparators() }.filter { it.isNotEmpty() }
-            .windowed(size = 2, step = 1)
-            .fold<List<Elements>, Numbers>(emptyList()) { acc, row -> acc + computeWindow(row.first(), row.last()) }
-            .distinct()
-            .sumOf { it.value }
+    fun part1(input: List<String>) = input.asSequence().parse().partNumbers()
+        .sumOf { it.first.value }
 
-    fun part2(input: List<String>) = 0
+    fun part2(input: List<String>) = input.asSequence().parse().partNumbers()
+        .groupBy(
+            keySelector = { it.second },
+            valueTransform = { it.first }
+        )
+        .filterKeys { it.value == '*' }
+        .filterValues { it.size == 2 }
+        .map { (_, v) -> v[0] * v[1] }
+        .sum()
 
-    // test if implementation meets criteria from the description, like:
+    // checking test inputs
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 4361)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 467835)
 
+    // print solutions
     val input = readInput("Day03")
     part1(input).println()
     part2(input).println()
@@ -20,9 +25,13 @@ fun main() {
 
 sealed class Element {
     data class Number(val rowIndex: Int, val value: Int, val range: IntRange) : Element()
-    data class Symbol(val value: Char, val position: Int) : Element()
+    data class Symbol(val rowIndex: Int, val value: Char, val position: Int) : Element()
     data class Separator(val value: String = "") : Element()
 }
+typealias NumberWithSymbol = Pair<Element.Number, Element.Symbol>
+typealias NumbersWithSymbols = List<NumberWithSymbol>
+
+operator fun Element.Number.times(other: Element.Number) = this.value * other.value
 
 fun Element.Number.expandedRange() =
     (range.first - 1).coerceAtLeast(0)..(range.last + 1).coerceAtMost(140)
@@ -48,21 +57,28 @@ fun Elements.lastIfNumberOrNull() : Element.Number? = when(lastOrNull()) {
 
 fun Char.isDot() = this == '.'
 fun Char.number(rowIndex: Int, idx: Int) = Element.Number(rowIndex, digitToInt(), idx..idx)
-fun Char.symbol(idx: Int) = Element.Symbol(this, idx)
+fun Char.symbol(rowIndex:Int, idx: Int) = Element.Symbol(rowIndex, this, idx)
 fun sep() = Element.Separator()
 
 fun String.parse(rowIndex: Int): Elements = foldIndexed(listOf()) { idx, acc, c -> when {
     c.isDigit() -> acc.lastIfNumberOrNull()?.let { lastNum ->
         acc.replaceLast(lastNum.update(c.number(rowIndex, idx))) } ?: (acc + c.number(rowIndex, idx))
     c.isDot() -> acc.lastIfNumberOrNull()?.let { acc + sep() } ?:acc
-    else -> acc + c.symbol(idx)
+    else -> acc + c.symbol(rowIndex, idx)
 }}
 
 fun Numbers.adjacentTo(symbol: Element.Symbol) = filter { symbol.position in it.expandedRange() }
 
-fun computeWindow(first: Elements, second: Elements): Numbers {
-    val collectFirstAdjacent = {acc: Numbers, symbol: Element.Symbol -> acc + first.numbers().adjacentTo(symbol) }
-    val collectSecondAdjacent = {acc: Numbers, symbol: Element.Symbol -> acc + second.numbers().adjacentTo(symbol) }
+// here is the meat of this solution
+fun Sequence<String>.parse() = mapIndexed { idx, row -> row.parse(idx).filterOutSeparators() }.filter { it.isNotEmpty() }
+
+fun Sequence<Elements>.partNumbers() = windowed(size = 2, step = 1)
+    .fold<List<Elements>, NumbersWithSymbols>(emptyList()) { acc, row -> acc + computeWindow(row.first(), row.last()) }
+    .distinct()
+
+fun computeWindow(first: Elements, second: Elements): NumbersWithSymbols {
+    val collectFirstAdjacent = {acc: NumbersWithSymbols, symbol: Element.Symbol -> acc + first.numbers().adjacentTo(symbol).map { it to symbol } }
+    val collectSecondAdjacent = {acc: NumbersWithSymbols, symbol: Element.Symbol -> acc + second.numbers().adjacentTo(symbol).map { it to symbol } }
 
     val a = first.symbols().fold(emptyList(), collectFirstAdjacent) + second.symbols().fold(emptyList(), collectFirstAdjacent)
     val b = first.symbols().fold(emptyList(), collectSecondAdjacent) + second.symbols().fold(emptyList(), collectSecondAdjacent)
